1e315936809978660a28ddb017a24ca12224d6c7
Ismael Febles <reynaldofebles@gmail.com>
2020-02-13 17:32:14 -0400

    Added score differentiation to KINGs for the bot - Adding relevance to moves making the piece to be killed next turn.

--- bot.js
+++ bot.js
@@ -25,16 +25,26 @@ const getOponentPlayablePieces = (board, player, playerStartsTop) => {
 
 
 /**
- * Simulate a play and get the oponent quantified sum as the returned value
+ * Simulate a play and get the oponent quantified sum (score) as the returned value and value indicating if can be killed 
  * @param {string[][]} board 
  * @param {string} player 
  * @param {boolean} startsTop 
- * @param {number} piece 
+ * @param {{
+    piece: number[],
+    index: number,
+    options: {
+        coordinate: number[],
+        killedPieces: number[][],
+    }[],
+  }} playablePiece 
  * @param {number} option 
  * @param {number} amountOfSimulations 
  */
-const simulatePlay = (board, player, startsTop, piece, option, amountOfSimulations = 1) => {
+const simulatePlay = (board, player, startsTop, playablePiece, option, amountOfSimulations = 1) => {
   const boardCopy = [...board].map(m => [...m]);
+  const piece = playablePiece.index;
+  const pieceIntendedCoordinate = playablePiece.options[option].coordinate;
+
   moves.managePlay(boardCopy, player, startsTop)
     .select(piece)
     .execute(option);
@@ -42,6 +52,9 @@ const simulatePlay = (board, player, startsTop, piece, option, amountOfSimulatio
   const oponent = playerList.find(f => f !== player);
   const oponentPlayablePieces = getOponentPlayablePieces(boardCopy, player, startsTop);
   const oponentQuantifiedPlayResult = calculateBestPlays(boardCopy, oponent, !startsTop, oponentPlayablePieces, true);
+
+  const killable = oponentPlayablePieces.some(s => s.options.some(o => o.killedPieces
+    .some(k => k[0] === pieceIntendedCoordinate[0] && k[1] === pieceIntendedCoordinate[1])));
 
   let result = oponentQuantifiedPlayResult
     .reduce((o, e) => o += e.points, 0);
@@ -51,7 +64,7 @@ const simulatePlay = (board, player, startsTop, piece, option, amountOfSimulatio
     const summedResults = oponentPlayablePieces.reduce((o, e) => {
       e.options.forEach((_f, i) => {
         caseCounter++;
-        o += simulatePlay(boardCopy, oponent, !startsTop, e.index, i, amountOfSimulations - 1);
+        o += simulatePlay(boardCopy, oponent, !startsTop, e, i, amountOfSimulations - 1).result;
       });
 
       return o;
@@ -61,7 +74,7 @@ const simulatePlay = (board, player, startsTop, piece, option, amountOfSimulatio
       result -= summedResults / caseCounter;
   }
 
-  return result;
+  return { result, killable };
 };
 
 
@@ -84,6 +97,8 @@ const calculateBestPlays = (board, player, startsTop, piecesOptions, simulating 
    */
   const threatened = [];
 
+  const pieceIsKing = coordinates => board[coordinates[0]][coordinates[1]] === board[coordinates[0]][coordinates[1]].toUpperCase();
+
   // Check for the ones with secured kills
   piecesOptions
     .filter(f => f.options.some(s => s.killedPieces.length))
@@ -93,7 +108,7 @@ const calculateBestPlays = (board, player, startsTop, piecesOptions, simulating 
         if (e.killedPieces.length)
           o.push({
             index: i,
-            points: 2 * e.killedPieces.length,
+            points: (e.killedPieces.some(s => pieceIsKing(s)) ? 3 : 2) * e.killedPieces.length,
           });
 
         return o;
@@ -107,7 +122,7 @@ const calculateBestPlays = (board, player, startsTop, piecesOptions, simulating 
       .some(k => k[0] === f.piece[0] && k[1] === f.piece[1]))))
     .forEach(f => threatened.push({
       index: f.index,
-      points: 2 * oponentOptions
+      points: (pieceIsKing(f.piece) ? 3 : 2) * oponentOptions
         .filter(oponent => oponent.options.some(option => option.killedPieces
           .some(k => k[0] === f.piece[0] && k[1] === f.piece[1])))
         .reduce((o, e) => {
@@ -137,8 +152,7 @@ const calculateBestPlays = (board, player, startsTop, piecesOptions, simulating 
   // Check if the piece can become a KING
   piecesOptions
     .filter(f =>
-      // If not a KING:
-      board[f.piece[0]][f.piece[1]] === board[f.piece[0]][f.piece[1]].toLowerCase()
+      !pieceIsKing(f.piece)
       && f.options.some(s => s.coordinate[0] === (startsTop ? emptyArray.length - 1 : 0)))
     .forEach(f => priority.push({
       index: f.index,
@@ -182,22 +196,33 @@ const calculateBestPlays = (board, player, startsTop, piecesOptions, simulating 
 
   // Evaluate play convenience regarding the "quantified play quality" of the oponent depending on the action taken
   if (!simulating) {
-    const consequenceResult = result.map(m => ({
+    const getPlayablePiece = index => piecesOptions.find(f => f.index === index);
+
+    const simulatedResults = result.map(m => ({
       ...m,
-      oponentPlayValue: simulatePlay(board, player, startsTop, m.piece, m.option),
+      oponentPlayValue: simulatePlay(board, player, startsTop, getPlayablePiece(m.piece), m.option),
+    }));
+
+    const consequenceResult = simulatedResults.map(m => ({
+      ...m,
+      // Increase the "oponentPlayValue" if the piece can be killed
+      alteredPlayValue: m.oponentPlayValue.result * (m.oponentPlayValue.killable ?
+        // If the piece is KING, set the value multiplier to 3; multiply by 2 if not
+        (pieceIsKing(getPlayablePiece(m.piece).piece) ? 3 : 2) :
+        1),
     }));
 
     const playValues = consequenceResult
-      .map(m => m.oponentPlayValue)
+      .map(m => m.alteredPlayValue)
       .filter((e, i, o) => o.indexOf(e) === i)
       .sort((a, b) => a - b)
-      .map((m, i) => ({ value: m, penalty: i - 3 }));
+      .map((m, i) => ({ value: m, penalty: i - 2 }));
 
     const penaltyList = consequenceResult
       .map(m => ({
         piece: m.piece,
         option: m.option,
-        penalty: playValues.find(f => f.value === m.oponentPlayValue).penalty,
+        penalty: playValues.find(f => f.value === m.alteredPlayValue).penalty,
       }));
 
     result.forEach(f => f.points -= penaltyList
